a416d4ba4d28d6ed719c8a4688357f629ed9324c
Juh <ricardoluizjr@live.com>
2023-06-20 15:25:30 -0300

    new file:   crud.php 	new file:   img/background-css.jpg 	new file:   img/editar.png 	new file:   img/excluir.png 	new file:   img/logo.png 	new file:   index.php 	new file:   scripts.js 	new file:   styles.css 	new file:   tabela.sql

---
+++ crud.php
@@ -0,0 +1,7 @@
+<?php
+
+//dados de conexao com banco de dados do sistema
+$host   = "localhost";
+$user   = "root";
+$pass   = "";
+$db     = "db_crud_teste";
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Agenda - Crud</title>
+    <link rel="stylesheet" href="styles.css">   
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arvo:ital@1&family=Flow+Circular&family=Kanit:wght@200&family=Tsukimi+Rounded&display=swap" rel="stylesheet">
+ </head>
+
+<body>
+    <nav id="msg-php" class="nav">
+        <img src="img/logo.png" alt="" id="logo">
+        <h2>Agenda - CRUD <p>by じゅ</p></h2>
+    </nav>
+
+    <form method="POST" id="crudForm">
+    <section id=sctDado>
+        <input id="nome" type=text class="inputText" required placeholder="Digite seu nome" name=nome />
+        <input id="email" type=email class="inputText" placeholder="Informe seu email" name=email/>
+        <input id="telefone" type=number class="inputText" placeholder="Digite seu telefone" name=telefone/>
+    </section>
+    <section id=sctButton>
+        <input id="id" type=hidden value="-1" />
+        <input type=reset class="button" id="btnClean" value="Limpar"/>
+        <input type=submit class="button"id="btnSave" value="Salvar"/>
+    </section>
+    </form>
+    <section id="divList">
+        <table>Contatos
+            <thead>
+                <tr>
+                    <th id="thCode">Código</th>
+                    <th id="thName">Nome</th>
+                    <th id="thEmail">Email</th>
+                    <th id="thPhone">Telefone</th>
+                    <th class="thBtn">Editar</th>
+                    <th class="thBtn">Remover</th>
+                </tr>
+            </thead> 
+            <tbody id="tbodyContacts">      
+                </tr>         
+            </tbody>
+        </table>
+    </section>
+
+</body>
+</html>
